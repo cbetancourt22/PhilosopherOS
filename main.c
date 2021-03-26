d95eaef24b0d2530fcf1d21287047749227f24ce
xavi9904 <xavi9904@hotmail.com>
2021-03-26 11:19:38 -0700

    Added main

---
+++ main.c
@@ -0,0 +1,23 @@
+// CS475W lab04 Threads
+// Part 1 of this lab is to use any data structure to represent the availability of process identifiers
+// Part 2 of this lab is to write a multithreaded program to test the solution of part 1 
+// Due time: 3/17/2021 Thursday
+// Student name: Bishesh Tuladhar, Xavier Betancourt
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <pthread.h>
+
+int main(void)
+{
+    printf("Hola BIshesh hehhe");
+    
+    
+    return 0;
+}
+
+
+
