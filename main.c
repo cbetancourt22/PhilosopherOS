772fce2b1e5f29d033862686c99b93373ece937d
xavi9904 <xavi9904@hotmail.com>
2021-03-30 21:31:56 -0700

    Added a thinking method

--- main.c
+++ main.c
@@ -1,23 +1,40 @@
-// CS475W lab04 Threads
-// Part 1 of this lab is to use any data structure to represent the availability of process identifiers
-// Part 2 of this lab is to write a multithreaded program to test the solution of part 1 
-// Due time: 3/17/2021 Thursday
+// CS475W lab05 Mutual Exclusion
+// Simulation of process for 'x' amount of time
+// Due time: 4/1/2021 Thursday
 // Student name: Bishesh Tuladhar, Xavier Betancourt
 
 #include <stdio.h>
 #include <unistd.h>
-#include <string.h>
 #include <stdlib.h>
-#include <sys/wait.h>
 #include <pthread.h>
+#include <time.h>
+
+#define NUM_PHILOSOPHERS 5 // 5 philosophers, numbered 0 to 4
+
+void think(int);
+void pickUpChop(int);
+void eat(int);
+void putDownChop(int);
+
+pthread_mutex_t chopsticks[NUM_PHILOSOPHERS];
+pthread_t philosophers[NUM_PHILOSOPHERS];
+pthread_attr_t attribute[NUM_PHILOSOPHERS];
 
 int main(void)
 {
-    printf("Hola BIshesh hehhe");
-    
+    printf("HOla bishesh");
     
     return 0;
 }
+
+void think(int philosopherIndex)
+{
+    int thinkingTime = rand() % 4 + 1; // philosopher thinks 1 - 3 seconds
+    printf("Philosopher %d thinks for %d seconds\n", philosopherIndex, thinkingTime);
+    sleep(thinkingTime);  
+}
+
+
 
 
 
